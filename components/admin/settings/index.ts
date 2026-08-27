b2de164e45e5fe60fc50286ff63bfdd0ca20f2cc
Legis Connect <instsermelhor.adm@gmail.com>
2026-08-27 00:05:44 -0300

    Create index.ts

---
+++ components/admin/settings/index.ts
@@ -0,0 +1,7 @@
+export { LegalDocuments } from './LegalDocumentsSettings';
+export { AdminUsers } from './AdminUsersSettings';
+export { GeneralSettings } from './GeneralSettings';
+export { ServiceGroupsSettings } from './ServiceGroupsSettings';
+export { LegalCodesSettings } from './LegalCodesSettings';
+export { DatabaseSettings } from './DatabaseSettings';
+export { APIConnections } from './APIConnections';
